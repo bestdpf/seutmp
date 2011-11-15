7b8fb51527f3b7778ee9c17b3b75d6dabb890c1f
Bolu Zhao <egret8807@126.com>
2011-11-15 12:54:53 +0800

    seu3d-1.0.33

--- src/agent/start.sh
+++ src/agent/start.sh
@@ -4,12 +4,11 @@
 #
 AGENT_BINARY="seu-spark-agent"
 BINARY_DIR="./"
-NUM_PLAYERS=5
-TEAM_NAME="SEU3D"
-#killall -9 "$AGENT_BINARY" &> /dev/null
+NUM_PLAYERS=9
+killall -9 "$AGENT_BINARY" &> /dev/null
 export LD_LIBRARY_PATH=./libs/:$LD_LIBRARY_PATH;
 for ((i=1;i<=$NUM_PLAYERS;i++)); do
 echo "Running agent No. $i"
-"$BINARY_DIR/$AGENT_BINARY" -s $1 -t $TEAM_NAME -u $i > /dev/null 2> /dev/null&
+"$BINARY_DIR/$AGENT_BINARY" -s $1 -t SEU3D -u $i > /dev/null 2> /dev/null&
 sleep 2
 done
